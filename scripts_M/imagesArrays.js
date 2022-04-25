4a77b46dd310aa57ae174143b568e5389430a31d
hilashor <hilashor@gmail.com>
2022-04-25 17:10:52 +0300

    onclick navigation bar

---
+++ scripts_M/imagesArrays.js
@@ -0,0 +1,67 @@
+// Games images by theme
+    // MATH
+    var math1cards = [
+      ["images_M/math/list1_math/2plus2.png" , 4],
+      ["images_M/math/list1_math/4.png" , 4],
+      ["images_M/math/list1_math/3plus6.png" , 9],
+      ["images_M/math/list1_math/9.png" , 9],
+      ["images_M/math/list1_math/4plus3.png" , 7],
+      ["images_M/math/list1_math/7.png" , 7],
+      ["images_M/math/list1_math/4plus9.png" , 13],
+      ["images_M/math/list1_math/13.png" , 13],
+      ["images_M/math/list1_math/5plus7.png" , 12],
+      ["images_M/math/list1_math/12.png" , 12],
+      ["images_M/math/list1_math/7plus4.png" , 11],
+      ["images_M/math/list1_math/11.png", 11]
+    ];
+    
+    var math2cards = [];
+    
+        //CLASSIC
+    var classic1cards = [
+      ["https://publicdomainvectors.org/tn_img/princess-in-red-dress.webp" , 1],
+      ["https://publicdomainvectors.org/tn_img/princess-in-red-dress.webp" , 1],
+      ["https://publicdomainvectors.org/tn_img/tropical-coconut-cocktail.webp" , 2],
+      ["https://publicdomainvectors.org/tn_img/tropical-coconut-cocktail.webp" , 2],
+      ["https://publicdomainvectors.org/tn_img/baby-with-lolipop.webp" , 3],
+      ["https://publicdomainvectors.org/tn_img/baby-with-lolipop.webp" , 3],
+      ["https://publicdomainvectors.org/tn_img/screaming-teenage-girl.webp" , 4],
+      ["https://publicdomainvectors.org/tn_img/screaming-teenage-girl.webp" , 4],
+      ["https://publicdomainvectors.org/tn_img/woman-surfboard-public-domain-vector.webp" , 5],
+      ["https://publicdomainvectors.org/tn_img/woman-surfboard-public-domain-vector.webp" , 5],
+      ["https://publicdomainvectors.org/tn_img/man-playing-violin-publicdomain.org.webp" , 6],
+      ["https://publicdomainvectors.org/tn_img/man-playing-violin-publicdomain.org.webp", 6]
+    ];
+    
+    var classic2cards = [
+      ["images_M/classic/list2_classic/bow.png" , 1],
+      ["images_M/classic/list2_classic/bow.png" , 1],
+      ["images_M/classic/list2_classic/camel.png" , 2],
+      ["images_M/classic/list2_classic/camel.png" , 2],
+      ["images_M/classic/list2_classic/cow_face.png" , 3],
+      ["images_M/classic/list2_classic/cow_face.png" , 3],
+      ["images_M/classic/list2_classic/halfway.png" , 4],
+      ["images_M/classic/list2_classic/halfway.png" , 4],
+      ["images_M/classic/list2_classic/pyramid.png" , 5],
+      ["images_M/classic/list2_classic/pyramid.png" , 5],
+      ["images_M/classic/list2_classic/twisted_monkey.png" , 6],
+      ["images_M/classic/list2_classic/twisted_monkey.png", 6]
+    ];
+    
+        //READING
+    var reading1cards = [
+      ["images_M/reading/list1_reading/bad_p.png" , 1],
+      ["images_M/reading/list1_reading/bad.png" , 1],
+      ["images_M/reading/list1_reading/bed_p.png" , 2],
+      ["images_M/reading/list1_reading/bed.png" , 2],
+      ["images_M/reading/list1_reading/desk_p.png" , 3],
+      ["images_M/reading/list1_reading/desk.png" , 3],
+      ["images_M/reading/list1_reading/egg_p.png" , 4],
+      ["images_M/reading/list1_reading/egg.png" , 4],
+      ["images_M/reading/list1_reading/hen_p.png" , 5],
+      ["images_M/reading/list1_reading/hen.png" , 5],
+      ["images_M/reading/list1_reading/table_p.png" , 6],
+      ["images_M/reading/list1_reading/table.png", 6]
+    ];
+    
+    var reading2cards = [];
